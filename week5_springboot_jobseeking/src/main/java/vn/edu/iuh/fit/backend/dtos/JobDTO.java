203599df2d7367fb4e4395a3b261f423555a5118
tienminhtran <tientot36@gmail.com>
2024-12-06 17:20:37 +0700

    push fe dung boostrap

--- week5_springboot_jobseeking/src/main/java/vn/edu/iuh/fit/backend/dtos/JobDTO.java
+++ week5_springboot_jobseeking/src/main/java/vn/edu/iuh/fit/backend/dtos/JobDTO.java
@@ -1,8 +1,4 @@
-/*
- * @ {#} JobDTO.java   1.0     08/11/2024
- *
- * Copyright (c) 2024 IUH. All rights reserved.
- */
+
 
 package vn.edu.iuh.fit.backend.dtos;
 
@@ -13,12 +9,7 @@ import vn.edu.iuh.fit.backend.models.SkillLevel;
 import java.util.List;
 import java.util.Map;
 
-/*
- * @description:
- * @author: Tran Hien Vinh
- * @date:   08/11/2024
- * @version:    1.0
- */
+
 @Getter
 @Setter
 public class JobDTO {
